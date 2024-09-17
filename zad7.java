8da519e58feccb8e2d7eeec5e080124e221d9453
mariuszeq <igormajchrzak6b@gmail.com>
2024-09-17 23:21:06 +0200

    Create zad7.java
    dsadsaad

---
+++ zad7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź liczbę: ");
+        int number = scanner.nextInt();
+        scanner.close();
+        
+        if (isPrime(number)) {
+            System.out.println("Liczba jest pierwsza.");
+        } else {
+            System.out.println("Liczba nie jest pierwsza.");
+        }
+    }
+    
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        if (num <= 3) return true;
+        if (num % 2 == 0 || num % 3 == 0) return false;
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+}
