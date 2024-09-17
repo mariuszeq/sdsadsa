8ce36e3619396441c556e939965d4d3891996dab
mariuszeq <igormajchrzak6b@gmail.com>
2024-09-17 23:04:05 +0200

    Create zad1.java
    fdsfdsf

---
+++ zad1.java
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Pętla, która wypisuje wszystkie liczby dwucyfrowe parzyste
+    for (int i = 10; i <= 98; i += 2) {
+        cout << i << " ";
+    }
+    return 0;
+}
